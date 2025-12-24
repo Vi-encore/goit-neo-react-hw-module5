6c54e150d10bd398ef63a26d71eeb7a711cceffd
Viktoriia Matiushko <vikamatuchko772@gmail.com>
2025-12-24 17:04:08 +0200

    added env

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+
+// before keys names prefix VITE or will be undefined
+// console.log(import.meta.env.VITE_API_READ_ACCESS);
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return <>hello</>;
+}
+
+export default App;
