16d6c2e9cf654cb0a54301945f9e6b524ab5f807
Viktoriia Matiushko <vikamatuchko772@gmail.com>
2025-12-25 02:03:27 +0200

    working!

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,3 +1,11 @@
+import { Link } from "react-router-dom";
+import css from './NotFoundPage.module.css'
+
 export default function NotFoundPage() {
-  return <>Not Found</>;
+  return (
+    <div className={css.wrap}>
+      <h1>Oops! Page does not exist!</h1>
+      <Link to={"/"} className={css['return-link']}>Return to Homepage</Link>
+    </div>
+  );
 }
